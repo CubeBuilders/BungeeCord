82b1db9fd95127ba2864f7dc24dfbaa409f8052a
Siggi <7286178+Siggi88@users.noreply.github.com>
2022-08-08 19:37:28 +0000

    Add InterceptBrigadier

--- proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
+++ proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
@@ -688,6 +688,9 @@ public class DownstreamBridge extends PacketHandler
             }
         }
 
+        modified = true;
+        io.siggi.interceptbrigadier.InterceptBrigadier.intercept(con, commands.getRoot(), con.getPendingConnection().getVersion());
+
         if ( modified )
         {
             con.unsafe().sendPacket( commands );
